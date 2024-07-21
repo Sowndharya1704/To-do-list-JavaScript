4be156497378dc75d968709054b742aad2a71846
Sowndharya1704 <sowndharya1704@gmail.com>
2024-07-21 14:23:15 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
-function message() {
-  let item = document.getElementById('todolist').value
-  let text = document.createTextNode(item)
-  let newItem = document.createElement('li','button')
-  newItem.appendChild(text)
-  document.getElementById('todoList').appendChild(newItem)
+function getinput(){
+  var add=document.getElementById("listtask")
+  var input=document.getElementById("myinput")
+      var litem=document.createElement("li")
+      litem.innerHTML=input.value+"  "+"<button onclick='del(event)'>DELETE</button>"
+      if(input.value==null||input.value==""){
+          alert("Enter some text");
+      }
+      else{
+      add.append(litem);
+      }
+}
+
+function del(event){
+  event.target.parentElement.remove();
 }
